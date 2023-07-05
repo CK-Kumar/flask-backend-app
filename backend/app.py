f2ae5e1fbc4f9f04c8d950492b78fb770dc0b1ac
CK-Kumar <chandru0063@gmail.com>
2023-07-05 21:17:52 +0200

    Ai model added

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,19 @@
 from flask import Flask, request, jsonify, session
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+from flask import render_template
+from fastai.vision.all import *
 from config import ApplicationConfig
 from models import db, User
+from flask import render_template
+from pathlib import Path
+import pathlib
+from PIL import Image
+from io import BytesIO
+import base64
+
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
 
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
@@ -12,12 +24,12 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+#Labeling function required for load_learner to work
+def GetLabel(fileName):
+  return fileName.split('-')[0]
 
-
-
-@app.route("/")
-def index():
-    return "Hello, World!"
+learn = load_learner(Path('export.pkl')) #Import Model
+cors = CORS(app) #Request will get blocked otherwise on Localhost
 
 
 @app.route("/register", methods=["POST"])
@@ -39,11 +51,15 @@ def register_user():
 
     return jsonify({"message": "User registered successfully"}), 201
 
+
+
 @app.route("/users", methods = ["GET"])
 def get_all_user():
     users = User.query.all()  
     usernames = [{"email" : user.email, "id": user.id} for user in users]
     return jsonify({"usernames": usernames}), 200
+
+
 
 @app.route("/delete", methods=["DELETE"])
 def delete_user():
@@ -58,6 +74,27 @@ def delete_user():
     return jsonify({"message": "User deleted successfully"}), 200
 
 
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    base64_string = request.json.get('imageString')
+    image_bytes = base64.b64decode(base64_string)
+    img = PILImage.create(image_bytes)
+    label, _, probs = learn.predict(img)
+    prediction = f'{label} ({torch.max(probs).item()*100:.0f}%)'
+    return jsonify({
+        "label": label,
+        "prediction":(torch.max(probs).item()*100)
+    })
+   
+
+@app.route("/")
+def index():
+    return render_template("upload.html")
+
+@app.route('/result')
+def show_result():
+    return render_template('result.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
